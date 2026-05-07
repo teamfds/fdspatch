8840a037ed5ae2b7f5669068016c35f77addbdd1
lobatolobato <felipelobato.rj@gmail.com>
2026-05-07 15:01:14 -0300

    feat: make meu_saco.py's output cleaner
    - Remove '_PATCHNUM' from processed older patch files
    
    - Delete older patch's COL files after extracting into pac file
    
    - Move processed character's scripts and pacs from the current patch into Data/current/ leaving only the current patch's assets on the root of Data/

--- tools/meu_saco.py
+++ tools/meu_saco.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import sys
 import re
-
+import shutil
 
 sys.path.append(str(Path(__file__).parent.parent))
 import tools
@@ -10,29 +10,44 @@ import tools
 mod_root = Path(__file__).parent.parent.resolve()
 os.chdir(mod_root)
 
-script_path = './your_script.py'
-
-for root, dirs, files in os.walk(f"{mod_root}/res"):
+for root, _, files in os.walk(f"{mod_root}/res"):
     for file in files:
-        if re.match(r'BBS_.*\.uexp', file):
-            full_path = os.path.join(root, file)
-            unpack_dest = full_path.replace(".uexp", ".bbscript")
-            parse_dest = unpack_dest.replace(".bbscript", ".txt")
-
-            print(f"Processing: {full_path}")
-            print(f"            {unpack_dest}")
-            print(f"            {parse_dest}")
-
-            tools.bbspack(["extract", full_path, unpack_dest])
-            tools.bbscript(["parse", "--overwrite", "--game", "ggst", unpack_dest, parse_dest])
-
-            os.remove(unpack_dest)
-
-        elif re.match(r'COL_.*\.uexp', file):
-            full_path = os.path.join(root, file)
-            pac_dest = full_path.replace(".uexp", ".pac")
-
-            print(f"Extracting COL: {full_path}")
-            print(f"           -> {pac_dest}")
-
-            tools.bbspack(["extract", full_path, pac_dest])
+        if not re.match(r'((BBS)|(COL))_.*\.uexp', file):
+            continue
+        
+        uexp = os.path.join(root, file)
+        dest = ""
+        print(f"Processing: {uexp}")
+        
+        if file.startswith("BBS"):
+            uexp_unpack_dest = uexp.replace(".uexp", ".bbscript")
+            uexp_parse_dest = uexp_unpack_dest.replace(".bbscript", ".txt")
+            print(f"> Extracting to: {uexp_unpack_dest}")
+            print(f"> Parsing to:   {uexp_parse_dest}")
+            
+            # Extract and parse bbscript
+            tools.bbspack(["extract", uexp, uexp_unpack_dest])
+            tools.bbscript(["parse", "--overwrite", "--game", "ggst", uexp_unpack_dest, uexp_parse_dest])
+            os.remove(uexp_unpack_dest)
+            dest = uexp_parse_dest
+        elif file.startswith("COL"):
+            col_unpack_dest = uexp.replace(".uexp", ".pac")
+            print(f"> Extracting to: {col_unpack_dest}")
+            # Extract colision file
+            tools.bbspack(["extract", uexp, col_unpack_dest])
+            dest = col_unpack_dest
+            
+        # If is from older patch
+        if re.match(r"((BBS)|(COL))_.+_\d+\..+", file):
+            print("> Removing old patch assets:")
+            print(f">  uexp:   {uexp}")
+            print(f">  uasset: {uexp.replace(".uexp", ".uasset")}")
+            os.remove(uexp)
+            os.remove(uexp.replace(".uexp", ".uasset"))
+            
+            print("> Removing patch number from the processed file's name")
+            os.rename(dest, re.sub(r"_\d+\.", ".", dest))
+        else:
+            print("> Moving processed file from current patch into /current directory")
+            os.makedirs(f"{root}/current", exist_ok=True)
+            shutil.move(dest, f"{root}/current")
